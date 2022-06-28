af48a1a9997909611859fa2d77ad8c9c1b366a98
benzkittisak <benzkittisak@stv.ac.th>
2022-06-28 11:33:42 +0700

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Kittisak Panluea on 28/6/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I am Rich")
+                    .font(.title)
+                    .foregroundColor(.white)
+                .padding(25)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by Kittisak Panluea on 28/6/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
